d64a866d31667e69a43da8f5a53851887b189048
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-20 15:18:54 -0800

    finish leap function v.2:fix logic bug
    Co-authored-by: Andrew Lawson <alawson89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 function leap(year) {
-  if (year % 4 === 0 && year % 100 === 0) {
-    if (year % 400 === 0) {
-      sentence = 'It is a leap year!';
-    } else {
-      sentence = 'It is NOT a leap year!';
-    }
+  let sentence;
+  if (year % 4 === 0 || (year % 100 === 0 && year % 400 === 0)) {
+    sentence = 'It is a leap year!';
   } else {
     sentence = 'It is NOT a leap year!';
   }
