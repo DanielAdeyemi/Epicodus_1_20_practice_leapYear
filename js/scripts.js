2547587f0661629815f24ffc470378009bb4fdfa
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-20 15:08:37 -0800

    finish leap function v.1
    Co-authored-by: Andrew Lawson <alawson89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 function leap(year) {
-
+  if (year % 4 === 0 && year % 100 === 0) {
+    if (year % 400 === 0) {
+      sentence = 'It is a leap year!';
+    } else {
+      sentence = 'It is NOT a leap year!';
+    }
+  } else {
+    sentence = 'It is NOT a leap year!';
+  }
   return sentence;
 }
 
